5db29e88dcab4a2c98491e19d620a12bd5535124
Uzair Dhada <dhadauzair@gmail.com>
2019-10-23 00:59:35 +0530

    - Added 500,404,422 error codes.

--- Contacts/Contacts/Controllers/HomeScreenViewController.swift
+++ Contacts/Contacts/Controllers/HomeScreenViewController.swift
@@ -23,6 +23,7 @@ class HomeScreenViewController: UIViewController {
         self.contactsTableView.tableFooterView = UIView()
     }
 
+    //MARK:- Custom Methods
     func getContacts() {
         self.view.activityStartAnimating()
         API.contacts.apiRequestData(method: .get, params: ["":""]) { (result : Result<[Contact], APIRestClient.APIServiceError>) in
@@ -46,14 +47,25 @@ class HomeScreenViewController: UIViewController {
                 self.contactsTableView.reloadData()
                 self.view.activityStopAnimating()
             case .failure(let error):
+                switch error {
+                case .internalServerError500:
+                    self.alert(message: "Internal Server Error", title: "")
+                case .notFound404:
+                    self.alert(message: "Not Found", title: "")
+                case .validationErrors422:
+                    self.alert(message: "Validation Error", title: "")
+                default:
+                    self.alert(message: error.localizedDescription, title: "")
+                }
                 print(error.localizedDescription)
                 self.view.activityStopAnimating()
             }
         }
     }
     
-    func moveToContactDetailViewController() {
+    func moveToContactDetailViewController(withContactID contactId : Int) {
         let contactDetailViewController : ContactDetailViewController = Constants.Screen.storyboard.instantiateViewController(withIdentifier: "ContactDetailViewController") as! ContactDetailViewController
+        contactDetailViewController.contactId = contactId
         self.navigationController?.pushViewController(contactDetailViewController, animated: true)
     }
     
@@ -62,6 +74,7 @@ class HomeScreenViewController: UIViewController {
         self.navigationController?.pushViewController(addOrEditViewController, animated: true)
     }
     
+    //MARK:- IBOutlet Methods
     @IBAction func didSelectAddButton(_ sender: Any) {
         moveToAddOrEditContactsViewController()
     }
@@ -75,9 +88,9 @@ extension HomeScreenViewController : UITableViewDataSource, UITableViewDelegate 
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let carKey = sectionTitles[section]
-        if let carValues = contactDictionary[carKey] {
-            return carValues.count
+        let sectionKey = sectionTitles[section]
+        if let contactArray = contactDictionary[sectionKey] {
+            return contactArray.count
         }
             
         return 0
@@ -91,16 +104,21 @@ extension HomeScreenViewController : UITableViewDataSource, UITableViewDelegate 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell : ContactTableViewCell = tableView.dequeueReusableCell(withIdentifier: ContactTableViewCell.identifier, for: indexPath) as! ContactTableViewCell
         
-        let carKey = sectionTitles[indexPath.section]
-        if let carValues = contactDictionary[carKey] {
-            cell.configureCellUI(contact: carValues[indexPath.row])
+        let sectionKey = sectionTitles[indexPath.section]
+        if let contactArray = contactDictionary[sectionKey] {
+            cell.configureCellUI(contact: contactArray[indexPath.row])
         }
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        moveToContactDetailViewController()
+        let sectionKey = sectionTitles[indexPath.section]
+        if let contactArray = contactDictionary[sectionKey] {
+            if let contactId = contactArray[indexPath.row].id {
+                moveToContactDetailViewController(withContactID : contactId)
+            }
+        }
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
